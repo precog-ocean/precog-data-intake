1aa541a82bf9e60af5d07b4d22d644878e4f0d56
LeoBertini <leonardo.bertini@liverpool.ac.uk>
2026-01-27 17:32:05 +0000

    Code cleanup.

--- intake_OceanVarsDL.py
+++ intake_OceanVarsDL.py
@@ -180,7 +180,7 @@ if __name__=="__main__":
             iterable_dwnld.append(
                 (df_single, download_path))  # this is the iterable being passed to the download function with 2 args
 
-        iterable_dwnld = iterable_dwnld[0:4]  # this is for testing. #TODO delete this line in the future
+        #iterable_dwnld = iterable_dwnld[0:4]  # this is for testing. #TODO delete this line in the future
 
         with  ThreadPool(min(32, os.cpu_count() + 4)) as pool:
             pool.starmap(download_files, iterable_dwnld) #starmap unpacks the iterable args to function
